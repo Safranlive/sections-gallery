5d879d0a0a0561170a43aab351f15c896ec3b450
Safranlive <a.k.m.safran@gmail.com>
2026-02-12 15:24:45 +0530

    Add backend config/shopify.js for Sections Gallery

---
+++ backend/config/shopify.js
@@ -0,0 +1,14 @@
+const { shopifyApi, LATEST_API_VERSION } = require('@shopify/shopify-api');
+require('@shopify/shopify-api/adapters/node');
+
+const shopify = shopifyApi({
+  apiKey: process.env.SHOPIFY_API_KEY,
+  apiSecretKey: process.env.SHOPIFY_API_SECRET,
+  scopes: process.env.SHOPIFY_SCOPES?.split(',') || [],
+  hostName: process.env.SHOPIFY_APP_URL?.replace(/https?:\/\//, ''),
+  apiVersion: LATEST_API_VERSION,
+  isEmbeddedApp: true,
+  isCustomStoreApp: false,
+});
+
+module.exports = shopify;
